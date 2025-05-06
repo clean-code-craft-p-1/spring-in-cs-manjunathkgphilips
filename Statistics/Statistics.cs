10bbc153656eb9e27517b55703ba122b1dee7a44
K G, Manjunath <Manjunath.K.G@philips.com>
2025-05-06 15:24:21 +0530

    make unittest passed

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -5,9 +5,22 @@ namespace Statistics
 {
     public class StatsComputer
     {
-        public Stats CalculateStatistics(List<float> numbers)
+        public Stats CalculateStatistics(List<double> numbers)
         {
+            return new Stats
+            {
+                average = (numbers.Any()?numbers.Average():double.NaN),
+                max =(numbers.Any()? numbers.Max():double.NaN),
+                min = (numbers.Any()? numbers.Min():double.NaN),
+            };
             //Implement statistics here
         }
+    }
+
+    public class Stats
+    {
+        public double average;
+        public double max;
+        public double min;
     }
 }
